3ce7192d24122e6d984bfc8931552c6380500744
prony6 <115931014+prony6@users.noreply.github.com>
2024-12-25 13:42:43 +0300

    first commit to main

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,11 @@
+
+function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Page</h1>
+      <p>This is the admin page.</p>
+    </div>
+  )
+}
+
+export default AdminPage;
---
+++ src/app/blog/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SinglePostPage() {
+  return (
+    <div>
+      This is the SinglePost Page
+    </div>
+  )
+}
+
+export default SinglePostPage
